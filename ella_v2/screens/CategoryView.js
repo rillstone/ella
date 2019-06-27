9264f239573040af4f48771a83d15affbfdbedad
Charlie Rillstone <charlierillstone@gmail.com>
2019-06-27 15:35:48 +1200

    add real svg graphs

--- ella_v2/screens/CategoryView.js
+++ ella_v2/screens/CategoryView.js
@@ -7,29 +7,59 @@ import {
     TextInput,
     Platform,
     StatusBar,
-    Image
+    Image,
+    Dimensions,
+    ImageBackground,
+    TouchableOpacity,
+    ScrollView,
+    Animated
 } from "react-native";
+import { Defs, LinearGradient, Stop } from 'react-native-svg'
+import { LineChart, Grid } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
 import * as theme from '../theme';
-import payments from '../assets/payments.json';
 import Icon from 'react-native-vector-icons/Ionicons'
-import SvgAnimatedLinearGradient from 'react-native-svg-animated-linear-gradient'
-import Svg, {Circle, Rect } from 'react-native-svg'
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+const IS_IOS = Platform.OS === 'ios';
+function wp (percentage) {
+    const value = (percentage * viewportWidth) / 100;
+    return Math.round(value);
+}
 
-const DATE_OPTIONS = { weekday: 'short', month: 'short', day: 'numeric' };
+const slideHeight = viewportHeight * 0.15;
+const slideWidth = wp(85);
+const itemHorizontalMargin = wp(2);
+const HEADER_MAX_HEIGHT = 400;
+const HEADER_MIN_HEIGHT = Platform.OS === 'ios' ? 250 : 250;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+
+export const sliderWidth = viewportWidth;
+export const itemWidth = slideWidth + itemHorizontalMargin * 2;
 class Overview extends Component {
     mounted = false;
     constructor(props){
         super();
         this.state = {
-          errors: [],
-          transactions: []
+          scrollY: new Animated.Value(0),
+          scrollOp: 1,
         }
         this.props = props;
     
     }
 
 
-
+    _renderScrollViewContent() {
+        const data = Array.from({ length: 30 });
+        return (
+          <View style={styles.scrollViewContent}>
+            {data.map((_, i) => (
+              <View key={i} style={styles.row}>
+                <Text>{i}</Text>
+              </View>
+            ))}
+          </View>
+        );
+      }
     componentWillMount() {
         this.mounted = true;
         this.startHeaderHeight = 80
@@ -41,27 +71,188 @@ class Overview extends Component {
     }
 
     render() {
+        const data2 = [ 1,1,1,1,1,1 ]
+        const Gradient = () => (
+            <Defs key={'gradient'}>
+                <LinearGradient id={'gradient'} x1={'0'} y={'0%'} x2={'100%'} y2={'0%'}>
+                    <Stop offset={'0%'} stopColor={'#FCE38A'}/>
+                    <Stop offset={'100%'} stopColor={'#F38181'}/>
+                </LinearGradient>
+            </Defs>
+        )
 
+        const { navigation } = this.props;
+        const data = navigation.getParam('data', require('../assets/images/default_back.jpg'));
+        const chartColor = navigation.getParam('chartColor', '#FFF');
+        const lineData = navigation.getParam('lineD',data2)
+        const headerTranslate = this.state.scrollY.interpolate({
+            inputRange: [0, HEADER_SCROLL_DISTANCE],
+            outputRange: [0, -HEADER_SCROLL_DISTANCE],
+            extrapolate: 'clamp',
+          });
+          const chartTranslate = this.state.scrollY.interpolate({
+            inputRange: [0, HEADER_SCROLL_DISTANCE],
+            outputRange: [0, HEADER_SCROLL_DISTANCE*0.6],
+            extrapolate: 'clamp',
+          });
+      
+          const imageOpacity = this.state.scrollY.interpolate({
+            inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+            outputRange: [1, 1, 0],
+            extrapolate: 'clamp',
+          });
+          const imageTranslate = this.state.scrollY.interpolate({
+            inputRange: [0, HEADER_SCROLL_DISTANCE],
+            outputRange: [0, 100],
+            extrapolate: 'clamp',
+          });
+      
+          const titleScale = this.state.scrollY.interpolate({
+            inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+            outputRange: [1, 1, 0.8],
+            extrapolate: 'clamp',
+          });
+          const titleTranslate = this.state.scrollY.interpolate({
+            inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+            outputRange: [0, 0, -8],
+            extrapolate: 'clamp',
+          });
         return (
-            <SafeAreaView style={{ flex: 1, backgroundColor: '#f6f5f7' }}>
-                <View style={{ flex: 1.2 }}>
-                    <View style={styles.titleContain}>
-                        <Text style={styles.title}>Planner</Text> 
-                    </View>
-                </View>
-                <View style={{ flex: 5}}>
-                    <View style={styles.titleContain}>
-                        <Text onPress={() => this.props.navigation.navigate('Home')} style={styles.title}>Planner</Text> 
-                    </View>
-                </View>
+            <View style={styles.fill}>
 
-            </SafeAreaView>
+            <StatusBar hidden={true} />
+            <TouchableOpacity     onPress={() => {this.setState({scrollOp:0}); this.props.navigation.navigate('Home'); }}
+                                            style={{
+                                position: 'absolute',
+                                right: 25,
+                                top: 25,
+                                bottom: 0, zIndex: 999}}>
+                        <Icon 
+                            name="ios-close-circle"
+                            size={36}
+                            color="#FFF"
+
+                            
+                        />
+                        </TouchableOpacity>
+            <Animated.ScrollView
+              style={styles.fill}
+              scrollEventThrottle={1}
+              onScroll={Animated.event(
+                [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
+                { useNativeDriver: true },
+              )}
+            >
+              {this._renderScrollViewContent()}
+            </Animated.ScrollView>
+            <Animated.View
+              style={[
+                styles.header,
+                { transform: [{ translateY: headerTranslate }] },
+              ]}
+            >
+              <Animated.Image
+                style={[
+                  styles.backgroundImage,
+                  {
+                    opacity: this.state.scrollOp,
+                    transform: [{ translateY: imageTranslate }],
+                  },
+                ]}
+                source={data}
+              />
+                <Animated.View style={[
+                { transform: [{ translateY: chartTranslate }] },
+              ]}>
+               <LineChart 
+                style={ { height: 200, top: 100} }
+                data={ lineData }
+                showGrid={ 'false' }
+                curve={ shape.curveNatural }
+                contentInset={ { top: 20, bottom: 20 } }
+                svg={{
+                    strokeWidth: 3,
+                    stroke: chartColor,
+                    onPress: (a,b,c) => console.log(a,b,c)
+                }}
+            >
+
+                <Gradient/>
+            </LineChart>
+            </Animated.View>
+            </Animated.View>
+            <Animated.View
+              style={[
+                styles.bar,
+                {
+                  transform: [
+                    { scale: titleScale },
+                    { translateY: titleTranslate },
+                  ],
+                },
+              ]}
+            >
+              {/* <Text style={styles.title2}>Title</Text> */}
+            </Animated.View>
+          </View>
         );
     }
 }
 export default Overview;
 
 const styles = StyleSheet.create({
+    fill: {
+        flex: 1,
+      },
+      content: {
+        flex: 1,
+      },
+      header: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'transparent',
+        overflow: 'hidden',
+        height: HEADER_MAX_HEIGHT,
+      },
+      backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        width: null,
+        height: HEADER_MAX_HEIGHT,
+        resizeMode: 'cover',
+      },
+      bar: {
+        backgroundColor: 'transparent',
+        marginTop: Platform.OS === 'ios' ? 40 : 38,
+        height: 32,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+      },
+      title2: {
+        color: 'white',
+        fontSize: 18,
+      },
+      scrollViewContent: {
+        marginTop: HEADER_MAX_HEIGHT,
+      },
+      row: {
+        height: 40,
+        margin: 16,
+        backgroundColor: '#D3D3D3',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+
+
+
     container: {
         flex: 1,
         alignItems: 'center',
@@ -73,15 +264,67 @@ const styles = StyleSheet.create({
         paddingTop: 20,
         flex: 1
     },
+    slideInnerContainer: {
+        width: viewportWidth,
+        height: 200,
+        // paddingHorizontal: itemHorizontalMargin,
+        paddingBottom: 18,
+        flexDirection: 'row',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.4,
+        shadowRadius: 4,
+        elevation: 1,
+    },
+    image: {
+        // resizeMode: 'cover',
+        flex: 1,
+        height: undefined,
+        width: undefined,
+        // borderRadius: IS_IOS ? 12 : 0,
+        borderTopLeftRadius: 12,
+        borderBottomLeftRadius: 12
+    },
+    title: {
+        color: '#1E2127',
+        fontSize: 24,
+        fontWeight: 'bold',
+        letterSpacing: 0.5
+    },
+    subtitle: {
+        marginTop: 2,
+        color: '#1E2127',
+        fontSize: 16,
+        fontWeight: '500',
+    },
+    
     cardContainer: {
         backgroundColor: 'grey', 
         height: 100, 
         borderRadius: 7 
     },
+    textContainer: {
+        justifyContent: 'center',
+        flex: 2,
+        paddingTop: 20 - 12,
+        paddingBottom: 20,
+        paddingHorizontal: 16,
+        backgroundColor: 'white',
+        // borderTopRightRadius: 12,
+        // borderBottomRightRadius: 12
+    },
     title: {
         fontSize: theme.sizes.title,
         fontWeight: '800',
         color: theme.colors.gray,
+    },
+    imageContainer: {
+        flex: 1,
+        marginBottom: IS_IOS ? 0 : -1, // Prevent a random Android rendering issue
+        // backgroundColor: 'blue',
+        overflow: 'hidden',
+        // borderTopLeftRadius: 12,
+        // borderBottomLeftRadius: 12
     },
     subtitle: {
         fontSize: theme.sizes.subtitle,
@@ -141,13 +384,15 @@ const styles = StyleSheet.create({
         // marginBottom: 10
       },
       shadow: {
-        shadowColor: 'black',
-        shadowOffset: {
-          width: 0,
-          height: 0,
-        },
-        shadowOpacity: 0.2,
-        shadowRadius: 5,
-        elevation: 0,
-      },
+        position: 'absolute',
+        top: 0,
+        left: itemHorizontalMargin,
+        right: itemHorizontalMargin,
+        bottom: 18,
+        shadowColor: '#000',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 10 },
+        shadowRadius: 10,
+        borderRadius: 12
+    },
 });
