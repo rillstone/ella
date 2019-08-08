94c673d87bd33a8e4bbd6f4a287a3da1b250ffa0
Charlie Rillstone <charlierillstone@gmail.com>
2019-08-08 12:34:49 +1200

    ella-15 settings options

--- screens/Settings.js
+++ screens/Settings.js
@@ -11,39 +11,73 @@ import {
   TouchableOpacity,
   Image,
   Dimensions,
-   Animated, ScrollView
+   Animated, ScrollView,
+   Switch
 } from "react-native";
 
 import { Button, Avatar, Divider } from "react-native-elements";
 import * as theme from "../theme";
 import { getInset } from "react-native-safe-area-view";
+import { dispatch, connect, Provider } from "../store";
 import payments from "../assets/payments.json";
 import Icon from "react-native-vector-icons/Ionicons";
 import SvgAnimatedLinearGradient from "react-native-svg-animated-linear-gradient";
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get(
   "window"
 );
+
+const mapStateToProps = state => ({
+  user: state.user,
+  // put the stuff here you want to access from the global store
+  // then instead of calling it from "this.state.<var>" call it from "this.props.<var>"
+
+});
 const TOP_SAFE_AREA = Platform.OS === "ios" ? getInset("top") : 40;
 const HEADER_MIN_HEIGHT = 100;
 const HEADER_MAX_HEIGHT = 200;
 const list = [
   {
     title: "Linked Accounts",
-    icon: "ios-link"
-  },
-  {
-    title: "Personal Info",
-    icon: "ios-body"
+    icon: "account-balance-wallet",
+    color: theme.scheme.royal_blue,
+    toggle: false
   },
   {
     title: "Budget",
-    icon: "ios-pricetags"
+    icon: "attach-money",
+    color: theme.scheme.fuchsia_blue,
+    toggle: false
   },
   {
     title: "Authentication",
-    icon: "ios-key"
+    icon: "lock",
+    color: theme.scheme.green,
+    toggle: false
+  }, 
+  {
+    title: "Dark Mode",
+    icon: "invert-colors",
+    color: theme.scheme.dark_grey,
+    toggle: true
+  },  
+  {
+    title: "About",
+    icon: "info",
+    color: theme.scheme.cerise,
+    toggle: false
+  },  
+  {
+    title: "Contact",
+    icon: "mail",
+    color: theme.scheme.supernova,
+    toggle: false
+  },  
+  {
+    title: "Sign Out",
+    icon: "exit-to-app",
+    color: theme.scheme.crusta,
+    toggle: false
   },
-
 
 ];
 class Settings extends Component {
@@ -60,65 +94,7 @@ class Settings extends Component {
     this.props = props;
   }
 
-  renderMenu = () => {
-    return (
-      <TouchableOpacity
-          style={{
-            flexDirection: "row",
-            alignContent: "center",
-            justifyContent: "center",
-            alignItems: "center",
-            marginLeft: 20,
-            marginRight: 20,
-            paddingBottom: 20,
-            // marginTop: 40
-          }}
-          onPress={() => {
-            this.props.action();
-            this.props.signOut();
-          }}
-        >
-          <View
-            style={{
-              flex: 1,
-              alignContent: "center",
-              justifyContent: "center",
-              alignItems: "center"
-            }}
-          >
-            <Avatar
-              rounded
-              icon={{  name:'exit-to-app', color: 'white'}}
-              size="medium"
-              avatarStyle={{backgroundColor: theme.scheme.crusta}}
-            />
 
-          </View>
-          <View style={{ flex: 5, flexDirection: "column", marginLeft: 10 }}>
-            <Text
-              style={{
-                fontSize: 16,
-                fontWeight: "500",
-                color: theme.colors.gray
-              }}
-            >
-              {/* {this.state.firstname + ' ' + this.state.lastname} */}
-              Sign Out
-            </Text>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              alignContent: "flex-end",
-              justifyContent: "flex-end",
-              alignItems: "flex-end"
-            }}
-          >
-            <Icon name="ios-arrow-forward" size={26} color={"#E1E1E1"} />
-          </View>
-        </TouchableOpacity>
-    )
-  }
 
 
   componentWillMount() {
@@ -130,9 +106,75 @@ class Settings extends Component {
     for (var i = 1; i <= 75; i++) {
       this.array.push(i);
     }
+    console.log(this.props.user.photoURL);
   }
 
   render() {
+    const renderMenu = 
+      list&& list.map(item => (
+  
+        <TouchableOpacity
+            key={item.title}
+            style={{
+              flexDirection: "row",
+              alignContent: "center",
+              justifyContent: "center",
+              alignItems: "center",
+              marginLeft: 20,
+              marginRight: 20,
+              paddingBottom: 20,
+              // marginTop: 40
+            }}
+            onPress={() => {
+              // this.props.action();
+              // this.props.signOut();
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                alignContent: "center",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Avatar
+                rounded
+                icon={{  name:item.icon, color: 'white'}}
+                size="medium"
+                avatarStyle={{backgroundColor: item.color}}
+              />
+  
+            </View>
+            <View style={{ flex: 5, flexDirection: "column", marginLeft: 10 }}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: "500",
+                  color: theme.colors.gray
+                }}
+              >
+                {/* {this.state.firstname + ' ' + this.state.lastname} */}
+                {item.title}
+              </Text>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                alignContent: "flex-end",
+                justifyContent: "flex-end",
+                alignItems: "flex-end"
+              }}
+            >
+              {item.toggle? 
+              <Switch />
+              :
+              <Icon name="ios-arrow-forward" size={26} color={"#E1E1E1"} />
+              }
+            </View>
+          </TouchableOpacity>
+    ));
+    
     const headerHeight = this.scrollYAnimatedValue.interpolate(
       {
         inputRange: [0, (HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT)],
@@ -146,12 +188,6 @@ class Settings extends Component {
           extrapolate: 'clamp'
         });
 
-    // const headerBackgroundColor = this.scrollYAnimatedValue.interpolate(
-    //   {
-    //     inputRange: [0, (HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT)],
-    //     outputRange: ['#e91e63', '#1DA1F2'],
-    //     extrapolate: 'clamp'
-    //   });
     return (
   
 
@@ -169,7 +205,8 @@ class Settings extends Component {
             justifyContent: "center",
             alignItems: "center",
             margin: 20,
-            marginTop: 10
+            marginLeft: 30,
+            marginTop: 0
           }}
           onPress={() => {
 
@@ -186,9 +223,9 @@ class Settings extends Component {
             <Avatar
               rounded
               avatarStyle={{ backgroundColor: theme.scheme.cadet_blue }}
-              size="medium"
+              size="large"
               title={"CR"}
-              // source={{ uri: image === "" ? null : image }}
+              source={this.props.user.photoURL ? {uri:this.props.user.photoURL} : null}
             />
             {/* <Avatar
                   rounded
@@ -203,26 +240,29 @@ class Settings extends Component {
                 // }}
                 /> */}
           </View>
-          <View style={{ flex: 5, flexDirection: "column", marginLeft: 10 }}>
+          <View style={{ flex: 5, flexDirection: "column", marginLeft: 25 }}>
             <Text
               style={{
-                fontSize: 16,
+                fontSize: 20,
                 fontWeight: "500",
-                color: theme.colors.gray
+                color: theme.colors.gray,
+                paddingBottom: 10,
               }}
             >
               {/* {this.state.firstname + ' ' + this.state.lastname} */}
-              {'Charlie' + " " + 'Rillstone'}
+              {/* {'Charlie' + " " + 'Rillstone'} */}
+              {this.props.user.displayName}
             </Text>
             <Text
               style={{
-                fontSize: 12,
+                fontSize: 18,
                 fontWeight: "400",
                 color: "#5B7282"
               }}
             >
               {/* {this.state.email} */}
-              {'charlierillstone@gmail.com'}
+              {/* {'charlierillstone@gmail.com'} */}
+              {this.props.user.email}
             </Text>
           </View>
           <View
@@ -236,15 +276,8 @@ class Settings extends Component {
             <Icon name="ios-arrow-forward" size={26} color={"#E1E1E1"} />
           </View>
         </TouchableOpacity>
-            {this.renderMenu()}
-            {this.renderMenu()}
-            {this.renderMenu()}
-            {this.renderMenu()}
-            {this.renderMenu()}
-            {this.renderMenu()}
-            {this.renderMenu()}
-            {this.renderMenu()}
-            {this.renderMenu()}
+            {renderMenu}
+
         
           </ScrollView>
   
@@ -256,7 +289,7 @@ class Settings extends Component {
     );
   }
 }
-export default Settings;
+export default connect(mapStateToProps)(Settings);
 
 const styles = StyleSheet.create({
 
